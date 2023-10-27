5ea22889a9bf589fc2d24a3677b33e3a5d49a04e
gcanti <giulio.canti@gmail.com>
2023-10-27 08:05:23 +0200

    move dtslint.json

--- scripts/copy-package-json.ts
+++ scripts/copy-package-json.ts
@@ -1,40 +1,38 @@
-import { Effect, pipe, ReadonlyRecord } from "effect"
+import { Console, Effect, identity } from "effect"
 import * as path from "node:path"
 import * as FileSystem from "./FileSystem"
 
-const excludeEffectPackages = (deps: Record<string, string>): Record<string, string> => {
-  return ReadonlyRecord.filter(deps, (_, k) => !k.startsWith("@effect"))
-}
+const moveJson = (from: string, to: string, modify: (json: any) => any) =>
+  Console.log(`moving ${from} to ${to}`).pipe(
+    Effect.flatMap(() => FileSystem.readJsonFile(from)),
+    Effect.map(modify),
+    Effect.flatMap((json) => FileSystem.writeFile(to, JSON.stringify(json, null, 2)))
+  )
 
-const read = pipe(
-  FileSystem.readJsonFile("package.json"),
-  Effect.map((json: any) => ({
-    name: json.name,
-    version: json.version,
-    description: json.description,
-    main: "index.js",
-    bin: "index.js",
-    engines: json.engines,
-    dependencies: excludeEffectPackages(json.dependencies),
-    peerDependencies: excludeEffectPackages(json.peerDependencies),
-    repository: json.repository,
-    author: json.author,
-    license: json.license,
-    bugs: json.bugs,
-    homepage: json.homepage,
-    tags: json.tags,
-    keywords: json.keywords
-  }))
+const packageJson = moveJson("package.json", path.join("dist", "package.json"), (json: any) => ({
+  name: json.name,
+  version: json.version,
+  description: json.description,
+  main: "index.js",
+  bin: "index.js",
+  engines: json.engines,
+  dependencies: json.dependencies,
+  peerDependencies: json.peerDependencies,
+  repository: json.repository,
+  author: json.author,
+  license: json.license,
+  bugs: json.bugs,
+  homepage: json.homepage,
+  tags: json.tags,
+  keywords: json.keywords
+}))
+
+const dtslintJson = moveJson(
+  path.join("src", "dtslint.json"),
+  path.join("dist", "dtslint.json"),
+  identity
 )
 
-const pathTo = path.join("dist", "package.json")
-
-const write = (pkg: object) => FileSystem.writeFile(pathTo, JSON.stringify(pkg, null, 2))
-
-const program = pipe(
-  Effect.sync(() => console.log(`copying package.json to ${pathTo}...`)),
-  Effect.flatMap(() => read),
-  Effect.flatMap(write)
-)
+const program = packageJson.pipe(Effect.flatMap(() => dtslintJson))
 
 Effect.runPromise(program)
